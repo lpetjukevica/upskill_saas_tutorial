12424939124cc513d67149ebb1692965a4c02f93
Lana Petjukevica <lana.petjukevica@hotmail.com>
2017-12-08 05:04:20 +0000

    added custom urls

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,12 +4,12 @@ class ContactsController < ApplicationController
     end
     
     def create
-      @contact=Contact.new(contact_params)
+      @contact = Contact.new(contact_params)
       if @contact.save
         flash[:sucess] = "Message sent."
         redirect_to new_contact_path
         else
-          flash[:danger] =@contact.errors.full_messages.join(", ")
+          flash[:danger] = @contact.errors.full_messages.join(", ")
         redirect_to new_contact_path
       end
     end
